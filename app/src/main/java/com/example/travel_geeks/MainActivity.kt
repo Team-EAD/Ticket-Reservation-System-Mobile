93dea1b098afeb29304e0dcf8c7eb5c60421a7af
Heshan Rathsara Kotuwe Gedara <88552872+heshanRkotuwegedara@users.noreply.github.com>
2023-09-30 21:04:18 +0530

    Login,SignUp, Create Reservation, Account Deactivation Interfaces are created

---
+++ app/src/main/java/com/example/travel_geeks/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.travel_geeks
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.travel_geeks.ui.theme.Travel_geeksTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Travel_geeksTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Travel_geeksTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/travel_geeks/View_Reservation_Activity.java
@@ -0,0 +1,14 @@
+package com.example.travel_geeks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Reservation_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_reservation);
+    }
+}
