c7b314b84f9ca2c1961584df96bfdfd647c2d7be
Heshan Rathsara Kotuwe Gedara <88552872+heshanRkotuwegedara@users.noreply.github.com>
2023-10-10 08:13:47 +0530

    Create reservation and vie reservation java classes were created

--- app/src/main/java/com/example/travel_geeks/View_Reservation_Activity.java
+++ app/src/main/java/com/example/travel_geeks/View_Reservation_Activity.java
@@ -1,14 +1,24 @@
 package com.example.travel_geeks;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class View_Reservation_Activity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_reservation);
-    }
+import java.util.List;
+
+public class View_Reservation_Activity extends AppCompatActivity {
+            @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_view_reservation);
+
+            RecyclerView recyclerView = findViewById(R.id.recyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+            List<Reservation> reservations = ReservationAPI.getReservations();
+            ReservationAdapter adapter = new ReservationAdapter(reservations);
+            recyclerView.setAdapter(adapter);
+        }
 }
